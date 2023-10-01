460ee9474a996b31c47119157863c78360336be0
Zengqing Wu <75557839+wuzengqing001225@users.noreply.github.com>
2023-10-01 17:32:58 +0900

    Add files via upload

---
+++ Figure_Plot_from_CSV.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import Function_Plot
+import Function_Theoretical_Solution
+
+para_cost = [2, 2]
+para_a = 14
+para_d = 0.00333333333333
+para_beta = 0.00666666666666
+
+ideal_price_lb = [0, 0]
+ideal_price_ub = [0, 0]
+ideal_profit_lb = [0, 0]
+ideal_profit_ub = [0, 0]
+ideal_solution = [ideal_price_lb, ideal_price_ub, ideal_profit_lb, ideal_profit_ub]
+ideal_solution = Function_Theoretical_Solution.theoretical_upperbound(para_cost, para_a, para_d, para_beta)
+
+id = "Output/Record-231001-1725-gpt-4" # The location of csv file here
+csv_file = f"{id}/logs_decision_plot.csv"
+df_decision = pd.DataFrame(pd.read_csv(csv_file))
+
+Function_Plot.data_visulization([], df_decision, ideal_solution, output_folder=f"{id}/")
